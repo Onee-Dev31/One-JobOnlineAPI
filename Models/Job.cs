193ca68d60a259aa8268e00b3bd88b0c6055e2c9
Thanakorn DuangKaew <Thanakorn.Dua@gmmchannelholding.com>
2026-07-15 14:12:06 +0700

    Update Model Add jobs delete Closing date is required.

--- Models/Job.cs
+++ Models/Job.cs
@@ -36,7 +36,6 @@ namespace JobOnlineAPI.Models
         public int? ApplicantCount { get; set; }
         public DateTime? PostedDate { get; set; }
 
-        [Required(ErrorMessage = "Closing date is required.")]
         public DateTime? ClosingDate { get; set; }
 
         public int? CreatedBy { get; set; }
